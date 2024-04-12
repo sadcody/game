50f861414850430631a97572cbc18bf62556232f
sadcody <sadcody023@gmail.com>
2024-04-12 16:47:44 +0300

    Add icons to buttons , style  and a scoreboard

--- javascript.js
+++ javascript.js
@@ -3,7 +3,9 @@ let paperBtn = document.querySelector("#Paper");
 let scissorsBtn = document.querySelector("#Scissors");
 let h1 = document.querySelector("#winner");
 let hC = document.querySelector("#computerChoice")
-
+let hP = document.querySelector("#playerChoice");
+let hS = document.querySelector("#score");
+let hL = document.querySelector("#lose");
 
 rockBtn.addEventListener("click",()=>{
     let playerChoice = "Rock";
@@ -25,18 +27,36 @@ function getComputerChoice(){
 }
 
 
+let winCount = 0; // Initialize win count
+let loseCount = 0;
 
-function playGame(playerChoice,computerChoice){
+function playGame(playerChoice, computerChoice) {
     computerChoice = getComputerChoice();
-    let finalResult = winner(playerChoice,computerChoice);
-    hC.textContent = "Computer choose " +computerChoice;
+    let finalResult = winner(playerChoice, computerChoice);
+    hP.textContent = "You choose: " + playerChoice;
+    hC.textContent = "Computer choose: " + computerChoice;
     h1.textContent = finalResult;
+
+    if (finalResult === "You win!") {
+        winCount++; // Increment win count if player wins
+        updateScoreboard();
+    }
+    if (finalResult === "You lose"){
+        loseCount++;
+        updateLoseCount();
+    }
+
     console.log(computerChoice);
     console.log(playerChoice);
     console.log(finalResult);
 }
 
-
+function updateScoreboard() {
+    hS.textContent = "Win: " + winCount; // Update win count in the scoreboard
+}
+function updateLoseCount(){
+    hL.textContent = "Lose: " + loseCount;
+}
 
 function winner(playerChoice,computerChoice){
    
